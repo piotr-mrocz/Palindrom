68685692115c269c03cc1c416e656510d0565cae
Piotr Mroczkowski <80059641+piotr-mrocz@users.noreply.github.com>
2021-04-11 15:46:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        public bool Palindrom(string word)
+        {
+            
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            // Sprawdź czy string jest palindromem.
+
+            Console.Write("Wprowadź słowo: ");
+            string word = Console.ReadLine();
+            string reverseWord = new string(word.ToCharArray().Reverse().ToArray());
+
+            bool palindrom = word.ToLower().Equals(reverseWord.ToLower());
+            if (palindrom == true)
+            {
+                Console.WriteLine($"Wyraz \"{word}\" jest palindromem.");
+            }
+            else
+            {
+                Console.WriteLine($"Wyraz \"{word}\" nie jest palindromem.");
+            }
+        }
+    }
+}
